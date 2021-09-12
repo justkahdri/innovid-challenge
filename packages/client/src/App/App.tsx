99a27f423c9f8f67a6a4b9cedeb97b90008eca44
JustKahdri <joaco.rm512@gmail.com>
2021-09-11 23:13:36 -0300

    added drag function & close animation to windows

--- packages/client/src/App/App.tsx
+++ packages/client/src/App/App.tsx
@@ -1,24 +1,56 @@
-import * as React from "react";
+import React, {useRef, useState} from "react";
+import {AnimatePresence} from "framer-motion";
 
 import PCWindow from "./Desktop/PCWindow";
 import styles from "./App.module.scss";
 import Taskbar from "./Taskbar";
 
+interface Server {
+  name: string;
+  id: number;
+  mounted: boolean;
+}
+
 const App: React.FC = () => {
-  const servers = Array(4)
-    .fill("")
-    .map((_, i) => ({name: `Server${i + 1}`, id: i + 1}));
+  const [servers, setServers] = useState(
+    Array(4)
+      .fill("")
+      .map((_, i) => ({name: `Server #${i + 1}`, id: i + 1, mounted: true})),
+  );
   const toggleServers = () => {
     // TODO
     console.log("Should run all servers...");
   };
 
+  const dragZone = useRef(null);
+
+  const handleOnClose = (id: number) => {
+    setServers((prev) => {
+      let selected = prev.find((s) => s.id === id) as Server;
+
+      selected = {...selected, mounted: false};
+
+      return [...prev.filter((server) => server.id !== id), selected];
+    });
+  };
+
   return (
     <div className={styles.container}>
-      <main className={styles.desktop}>
-        {servers.map(({id, name}) => (
-          <PCWindow key={name} serverId={id} />
-        ))}
+      <main ref={dragZone} className={styles.desktop}>
+        <AnimatePresence>
+          {servers.map(
+            ({id, name, mounted}) =>
+              mounted && (
+                <PCWindow
+                  key={name}
+                  dragConstraints={dragZone}
+                  serverId={id}
+                  title={name}
+                  onClose={handleOnClose}
+                />
+              ),
+          )}
+        </AnimatePresence>
       </main>
       <Taskbar handleStart={toggleServers} />
     </div>
--- packages/client/src/App/Desktop/PCWindow.tsx
+++ packages/client/src/App/Desktop/PCWindow.tsx
@@ -1,4 +1,5 @@
 import React, {FC, useState, useEffect, useRef} from "react";
+import {motion} from "framer-motion";
 
 import getServerData from "../../utils/getServerData";
 import turnedOn from "../../assets/pc-on.gif";
@@ -6,7 +7,14 @@ import turnedOff from "../../assets/pc-off.png";
 
 import styles from "./PCWindow.module.scss";
 
-const PCWindow: FC<{serverId: number}> = ({serverId}) => {
+interface PCWindowProps {
+  onClose: (id: number) => void;
+  serverId: number;
+  title: string;
+  dragConstraints?: React.RefObject<Element>;
+}
+
+const PCWindow: FC<PCWindowProps> = ({onClose, title, serverId, dragConstraints}) => {
   const [isOnline, setIsOnline] = useState(false);
   const [load, setLoad] = useState<number>();
 
@@ -54,33 +62,48 @@ const PCWindow: FC<{serverId: number}> = ({serverId}) => {
   }, [key]);
 
   return (
-    <section className={`${styles.window} window`}>
-      <div className="title-bar">
-        <div className="title-bar-text">Server #{serverId}</div>
-        <div className="title-bar-controls">
-          <button aria-label="Minimize" />
-          <button aria-label="Maximize" />
-          <button aria-label="Close" />
+    <motion.div
+      drag
+      animate={{scale: 1}}
+      dragConstraints={dragConstraints}
+      dragElastic={false}
+      dragMomentum={false}
+      exit={{scale: 0}}
+      initial={{scale: 0}}
+      style={{width: "fit-content", height: "fit-content"}}
+    >
+      <section className={`${styles.window} window`}>
+        <div className="title-bar">
+          <div className="title-bar-text">{title}</div>
+          <div className="title-bar-controls">
+            <button aria-label="Minimize" />
+            <button aria-label="Maximize" />
+            <button
+              aria-label="Close"
+              onClick={() => onClose(serverId)}
+              onKeyPress={(e) => ["Enter", "Space"].includes(e.code) && onClose(serverId)}
+            />
+          </div>
         </div>
-      </div>
-      <div className="window-body">
-        {isOnline ? (
-          <img alt="computer" className={styles.computer} src={turnedOn} />
-        ) : (
-          <img alt="computer" className={styles.computer} src={turnedOff} />
-        )}
-      </div>
-      <div className={`status-bar `}>
-        <p className={`${styles.field} status-bar-field`}>Satus: {isOnline ? "ON" : "OFF"}</p>
-        <p
-          className={`${styles.field} ${styles["field--btn"]} status-bar-field`}
-          onClick={toggleServerStatus}
-        >
-          {isOnline ? "shut down" : "turn on"}
-        </p>
-        <p className="status-bar-field">CPU Usage: {load || "..."}%</p>
-      </div>
-    </section>
+        <div className="window-body">
+          {isOnline ? (
+            <img alt="computer" className={styles.computer} src={turnedOn} />
+          ) : (
+            <img alt="computer" className={styles.computer} src={turnedOff} />
+          )}
+        </div>
+        <div className={`status-bar `}>
+          <p className={`${styles.field} status-bar-field`}>Satus: {isOnline ? "ON" : "OFF"}</p>
+          <p
+            className={`${styles.field} ${styles["field--btn"]} status-bar-field`}
+            onClick={toggleServerStatus}
+          >
+            {isOnline ? "shut down" : "turn on"}
+          </p>
+          <p className="status-bar-field">CPU Usage: {load || "..."}%</p>
+        </div>
+      </section>
+    </motion.div>
   );
 };
 
