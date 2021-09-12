c0fcdc6a162e05a80bf4b48bbfb5146fe6f342cc
JustKahdri <joaco.rm512@gmail.com>
2021-09-12 00:28:20 -0300

    finished taskbar

---
+++ packages/client/src/App/Taskbar/constants.ts
@@ -0,0 +1,26 @@
+import github from "../../assets/github.png";
+import linkedin from "../../assets/linkedin.png";
+import readme from "../../assets/readme.png";
+import twitter from "../../assets/twitter.png";
+
+export const Apps = [
+  {link: "https://github.com/justkahdri", icon: github, name: "Github", isExternal: true},
+  {
+    link: "https://linkedin.com/in/joaquin-montes",
+    icon: linkedin,
+    name: "Linkedin",
+    isExternal: true,
+  },
+  {
+    link: "https://twitter.com/justkahdri",
+    icon: twitter,
+    name: "Twitter",
+    isExternal: true,
+  },
+  {
+    link: "#readme",
+    icon: readme,
+    name: "Readme",
+    isExternal: false,
+  },
+];
--- packages/client/src/App/Taskbar/index.tsx
+++ packages/client/src/App/Taskbar/index.tsx
@@ -1,40 +1,77 @@
-import React, {FC, MouseEventHandler} from "react";
+import React, {FC, MouseEventHandler, useEffect, useRef, useState} from "react";
 
 import logo from "../../assets/logo.png";
-import github from "../../assets/github.png";
+import sound from "../../assets/loudspeaker_rays-0.png";
+import miniWindows from "../../assets/accessibility_two_windows.png";
 
 import styles from "./Taskbar.module.scss";
+import {Apps} from "./constants";
 
 interface TaskbarProps {
   handleStart: MouseEventHandler;
 }
 
 const Taskbar: FC<TaskbarProps> = ({handleStart}) => {
+  const [time, setTime] = useState<string>();
+  const timeInterval = useRef<number>();
+
+  useEffect(() => {
+    const dateFormat = new Intl.DateTimeFormat("en", {
+      timeStyle: "short",
+    });
+
+    setTime(dateFormat.format(Date.now()));
+
+    timeInterval.current = setInterval(() => {
+      setTime(dateFormat.format(Date.now()));
+    }, 60000);
+
+    return () => {
+      clearInterval(timeInterval.current);
+    };
+  }, []);
+
   return (
     <header className={`${styles.taskbar} window`}>
-      {/* <div className="title-bar">
-          <div className="title-bar-text">A Window With Stuff In It</div>
-          <div className="title-bar-controls">
-            <button aria-label="Minimize" />
-            <button aria-label="Maximize" />
-            <button aria-label="Close" />
+      <nav className={`${styles["taskbar__content"]} window-body`}>
+        <div>
+          <button className={styles["taskbar__start"]} onClick={handleStart}>
+            <img className={styles["windows-logo"]} src={logo} />
+            Start
+          </button>
+          <hr />
+
+          {Apps.map(({link, icon, isExternal, name}) =>
+            isExternal ? (
+              <a
+                key={name}
+                aria-label={name}
+                className={styles["taskbar__icon"]}
+                href={link}
+                referrerPolicy="no-referrer"
+                rel="noreferrer"
+                target="_blank"
+              >
+                <img src={icon} />
+              </a>
+            ) : (
+              <a key={name} aria-label={name} className={styles["taskbar__icon"]} href={link}>
+                <img src={icon} />
+              </a>
+            ),
+          )}
+          <hr />
+        </div>
+        <div>
+          <hr />
+          <div className="status-bar">
+            <p className={`${styles["taskbar__info"]} status-bar-field`}>
+              <img alt="mini-windows" src={miniWindows} />
+              <img alt="sound-icon" src={sound} />
+              {time}
+            </p>
           </div>
-        </div> */}
-      <nav className={`${styles["taskbar--content"]} window-body`}>
-        <button className={styles["taskbar--start"]} onClick={handleStart}>
-          <img className={styles["windows-logo"]} src={logo} />
-          Start
-        </button>
-        <hr />
-        <a
-          className={styles["taskbar--icon"]}
-          href="https://github.com/justkahdri"
-          referrerPolicy="no-referrer"
-          rel="noreferrer"
-          target="_blank"
-        >
-          <img src={github} />
-        </a>
+        </div>
       </nav>
     </header>
   );
